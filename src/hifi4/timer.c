6c390ae4843ed4af9ae93ea8338a568bb311efd5
James Turton <james.turton@gmx.com>
2026-01-19 12:36:18 +0100

    hifi4: Add timer_reset to prevent timer wraparound/overflow

--- src/hifi4/timer.c
+++ src/hifi4/timer.c
@@ -10,6 +10,7 @@
 #include "board/misc.h" // timer_read_time
 #include "board/timer_irq.h" // timer_dispatch_many
 #include "sched.h" // DECL_INIT
+#include "board/irq.h" // irq_disable
 
 static uint32_t timer1_total_times;
 
@@ -21,10 +22,12 @@ static uint32_t timer1_total_times;
 void __visible __aligned(16)
 HSTIMER0_IRQHandler(uint32_t irq, void *arg)
 {
+    irq_disable();
     uint32_t next = timer_dispatch_many();
     uint32_t now = timer_read_time();
     int32_t diff = next - now;
     timer_set(diff);
+    irq_enable();
 }
 
 // Return the current time (in absolute clock ticks).
@@ -48,11 +51,27 @@ timer_set(uint32_t next)
 void
 timer_kick(void)
 {
-    timer_set(300);
+    timer_set(1);
 }
 
-void timer_reset(void)
+// Dummy timer to avoid scheduling a SysTick irq greater than 0xffffff
+static uint_fast8_t
+timer_wrap_event(struct timer *t)
 {
+    t->waketime += 0xffffff;
+    return SF_RESCHEDULE;
+}
+static struct timer wrap_timer = {
+    .func = timer_wrap_event,
+    .waketime = 0xffffff,
+};
+void
+timer_reset(void)
+{
+    if (timer_from_us(100000) <= 0xffffff)
+        // Timer in sched.c already ensures SysTick wont overflow
+        return;
+    sched_add_timer(&wrap_timer);
 }
 DECL_SHUTDOWN(timer_reset);
 
@@ -69,11 +88,11 @@ timer_hw_init(void)
 {
     timer1_total_times = 0;
 
-    hstimer_init(HSTIMER_1, 1);
+    hstimer_init(HSTIMER_1, 0);
     hstimer_start_periodic(HSTIMER_1, 0xffffffff, 0, HSTIMER1_IRQHandler, NULL);
     timer_reset();
 
-    hstimer_init(HSTIMER_0, 1);
+    hstimer_init(HSTIMER_0, 0);
     hstimer_start_oneshot(HSTIMER_0, 200000, 0, HSTIMER0_IRQHandler, NULL);
     timer_kick();
 }
